97c8d2f96f2e6236ac723416fdc4d86f5df3d7c0
Eric Stokes <fernferret@gmail.com>
2010-04-21 13:51:56 -0400

    apparently i changed this file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -41,7 +41,8 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   def current_user(stubs = {})
-    @current_user = mock()
+    @current_user = User.new
+    @current_user.stubs(:stubs)
     @current_user.stubs = stubs
 
   end
