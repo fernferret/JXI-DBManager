63c5c760cf174358fbdd0df0b0f2fbf07ab83867
David Pick <pickdavid@gmail.com>
2010-04-22 21:49:07 -0400

    done with columns controller

--- app/controllers/column_controller_controller.rb
+++ app/controllers/column_controller_controller.rb
@@ -1,17 +1,39 @@
 class ColumnControllerController < ApplicationController
   def index
+		@columns = Column.all
+
+		respond_to do |format|
+			format.html
+		end
   end
 
   def show
+		@column = Column.find(params[:id])
+
+		respond_to do |format|
+			formatl.html
+		end
   end
 
   def edit
+		@column = Column.find(params[:id])
   end
 
   def create
+		@column = Column.new(params[:column])
+		
+		respond_to do |format|
+			format.html { render :action => "new" }
+		end
   end
 
   def destroy
+		@column = Column.find(params[:id])
+		@column.destroy
+
+		respond_to do |format|
+			format.html { redirect_to(columns_url) }
+		end
   end
 
 end
