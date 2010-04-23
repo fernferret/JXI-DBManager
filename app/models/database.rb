b37b2cdafce0338840b91114fac78a579a105193
Eric Stokes <fernferret@gmail.com>
2010-04-22 23:54:50 -0400

    Database views done

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,8 +1,8 @@
 class Database < ActiveRecord::Base
   has_many :tables
   belongs_to :user
-  validates_presence_of :name, :user
-  validates_associated :user
+  # validates_presence_of :name, :user
+  # validates_associated :user
   validates_uniqueness_of :name, :scope => :user_id
   
   def exists_with_title(name)
