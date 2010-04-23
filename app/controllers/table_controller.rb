b14a349e76395fe1ada1c98ec4ce9dfa8828ea55
Eric Stokes <fernferret@gmail.com>
2010-04-23 00:23:45 -0400

    Started adding tables

---
+++ app/controllers/table_controller.rb
@@ -0,0 +1,50 @@
+class TableController < ApplicationController
+  def index
+		@database = Database.find(params[:database_id])
+		@tables = @database.tables
+
+		respond_to do |format|
+			format.html
+		end
+  end
+
+  def show
+		@database = Database.find(params[:database_id])
+		@table = @database.tables.find(params[:id])
+  end
+
+  def edit
+		@database = Database.find(params[:database_id])
+		@table = @database.tables.find(params[:id])
+  end
+
+  def new
+    @database = Database.find(params[:id])
+    @database.tables.build
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+		@database = Database.find(params[:database_id])
+		@table = @database.tables.build
+
+		if @table.save
+			redirect_to_table_url(@table)
+		else
+			render :action => "new"
+		end
+  end
+
+  def destroy
+		@database = Database.find(params[:database_id])
+		@table = @database.table.find(params[:id])
+		@table.destroy
+
+		respond_to do |format|
+			format.html { redirect_to_database_tables_path(@database) }
+		end
+  end
+
+end
