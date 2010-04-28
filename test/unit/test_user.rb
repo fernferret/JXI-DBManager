a9295c3771d01c2777fcb19dc3af8a5a18e60883
David Pick <pickdavid@gmail.com>
2010-04-28 13:52:27 -0400

    added more tests to the user

--- test/unit/test_user.rb
+++ test/unit/test_user.rb
@@ -48,4 +48,47 @@ class UserTest < ActiveSupport::TestCase
 #    user.name = "My Name"
     assert !user.valid?, 'Saved user without name'
 	end 
+
+	def test_user_has_email
+#    Sample use of the factory
+    user = Factory.build(:user)
+#    user = User.new
+#    user.username = "Username"
+#    user.password = "Password"
+#    user.name = "My Name"
+		user.email = "test@test.com"
+    assert_match /.*@.*\..*/, user.email, 'User did not have a vaild email address' 
+	end 
+
+	def test_user_is_admin
+#    Sample use of the factory
+    user = Factory.build(:user)
+#    user = User.new
+#    user.username = "Username"
+#    user.password = "Password"
+#    user.name = "My Name"
+		user.permissions = "admin"
+    assert_equals "admin", user.permissions, 'User was not an admin'
+	end 
+
+	def test_user_is_ta
+#    Sample use of the factory
+    user = Factory.build(:user)
+#    user = User.new
+#    user.username = "Username"
+#    user.password = "Password"
+#    user.name = "My Name"
+		user.permissions = "ta"
+    assert_equals "ta", user.permissions, 'User was not a ta'
+	end 
+
+	def test_user_is_regular_user
+#    Sample use of the factory
+    user = Factory.build(:user)
+#    user = User.new
+#    user.username = "Username"
+#    user.password = "Password"
+#    user.name = "My Name"
+    assert_equals "", user.permissions, 'User was not a regular user'
+	end 
 end
