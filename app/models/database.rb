dc4628583650a7362d15b88900249eca170c00ad
Eric Stokes <fernferret@gmail.com>
2010-04-29 15:49:31 -0400

    Trying again, passing all database tests

--- app/models/database.rb
+++ app/models/database.rb
@@ -4,12 +4,28 @@ class Database < ActiveRecord::Base
   validates_presence_of :name, :user
   # validates_associated :user
   validates_uniqueness_of :name, :scope => :user_id
-  
+
   def exists_with_title(name)
     return true
   end
-	
-	def destroy_database(user)
-		return true
-	end
+  
+  def destroy_database(user)
+    if self.user == user || user.permissions == "admin"
+      self.destroy
+    end
+  end
+
+  def edit_database(user, params)
+    if self.user == user || user.permissions == "admin"
+      self.update_attributes(params)
+    end
+  end
+
+  def view_database(user)
+    if self.user == user || user.permissions == "ta" || user.permissions == "admin"
+      return true
+    else
+      return false
+    end
+  end
 end
