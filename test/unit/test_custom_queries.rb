d8f130cf703f083d46c7724e442e1a58da9ac947
CSSE MacBook Pro 01 <cssembp01@cssembp-01.wlan.rose-hulman.edu>
2010-05-14 14:45:04 -0400

    Intermediate commit for custom sql and sharing fixes

--- test/unit/test_custom_queries.rb
+++ test/unit/test_custom_queries.rb
@@ -1,20 +1,20 @@
 require 'test/test_helper'
 
 class DatabaseTest < ActiveSupport::TestCase
+  
   def setup
     @userA = Factory.create(:user)
     @database = Factory.create(:database, :user => @userA)
   end
+  
   def test_custom_sql_query_success
     query = "SELECT * FROM *"
     assert_equal true, @database.issue_query(@userA, query)
   end
+  
   def test_custom_sql_query_fail
     query = ""
     assert_equal false, @database.issue_query(@userA, query)
   end
-  def test_user_sharing
-    db2 = Factory.create(:database, :user => @userA)
-
-  end
+  
 end
