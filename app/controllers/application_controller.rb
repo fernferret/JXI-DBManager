21b22bcf0539bae6b77feef79f93a2fe960e592b
David Pick <pickdavid@gmail.com>
2010-05-20 16:58:20 -0400

    added a drop down two switch languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,4 +25,8 @@ class ApplicationController < ActionController::Base
 	def set_locale
 		I18n.locale = params[:locale]
 	end
+
+	def default_url_options(options={})
+		{ :locale => I18n.locale }
+	end
 end
